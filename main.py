11fb63851ba63d0924ffcc3a5683e97cbd4ab514
futi7 <fuatseven1997@hotmail.com>
2020-10-28 19:55:46 +0300

    Initialized the script

---
+++ main.py
@@ -0,0 +1,17 @@
+class AnagramChecker:
+    def __init__(self, first_string, second_string):
+        self.first_string = first_string
+        self.second_string = second_string
+
+    def check_if_anagrams():
+        pass
+
+    def check_required_deletions():
+        pass
+
+
+if __name__ == "__main__":
+    first_string = input("Enter first string")
+    second_string = input("Enter second string")
+    anagramChecker = AnagramChecker(first_string, second_string)
+    print("First:"+anagramChecker.first_string+ "\n Second:"+anagramChecker.second_string)
